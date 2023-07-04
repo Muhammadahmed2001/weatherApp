500d38e488a3d2328f53a7a71de285bc1b634f80
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-07-05 01:03:22 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,23 +8,75 @@ let humitidy = document.getElementById("humitidy")
 let wind_speed = document.getElementById("wind_speed")
 let clouds = document.getElementById("clouds")
 
-navigator.geolocation.getCurrentPosition((location)=>{
+navigator.geolocation.getCurrentPosition((location) => {
     let lat = location.coords.latitude
     let lon = location.coords.longitude
-
+    
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=f4dce3879f81d250a6e851c7ceef9a94&units=metric`)
-        .then(data => data.json())
-        .then((data) => {
+    .then(data => data.json())
+    .then((data) => {
+            let weather_img = document.getElementById("weather_img")
             country.innerHTML = data.sys.country;
             city.innerHTML = `${data.name},${data.sys.country}`
-            date.innerHTML = moment().format("MMM Do YY");
-            temp.innerHTML = Math.round(data.main.temp)+"°C";
+            date.innerHTML = moment().format("ll");
+            temp.innerHTML = Math.round(data.main.temp) + "°C";
             weather.innerHTML = data.weather[0].main
-            feels_like.innerHTML = Math.round(data.main.feels_like)+"°C";
-            humitidy.innerHTML = data.main.humidity +"%"
-            wind_speed.innerHTML = data.wind.speed +"km/h"
-            clouds.innerHTML = data.clouds.all +"%"
+            feels_like.innerHTML = Math.round(data.main.feels_like) + "°C";
+            humitidy.innerHTML = data.main.humidity + "%"
+            wind_speed.innerHTML = data.wind.speed + "km/h"
+            clouds.innerHTML = data.clouds.all + "%"
+            if (data.weather[0].main == "Thunderstorm") {
+                weather_img.src = "pic/thunderstorms-rain.svg";
+            }
+            else if (data.weather[0].main == "Clouds") {
+                weather_img.src = 'pic/cloudy.svg';
+                
+                
+            }
+            else if (data.weather[0].main === "Rain") {
+                weather_img.src = "pic/extreme-night-rain.svg"
+
+            }
+            else if (data.weather[0].main === "Snow") {
+                weather_img.src = "pic/overcast-night-snow.svg"
+
+            }
+            else if (data.weather[0].main === "Mist") {
+                weather_img.src = "pic/mist.svg"
+
+
+            }
+            else if (data.weather[0].main === "Smoke") {
+                weather_img.src = "pic/smoke.svg"
+
+
+
+            }
+            else if (data.weather[0].main === "Haze") {
+                weather_img.src = "pic/haze.svg"
+
+
+            }
+            else if (data.weather[0].main === "Dust") {
+                weather_img.src = "pic/dust.svg"
+
+
+            }
+            else if (data.weather[0].main === "Fog") {
+                weather_img.src = "pic/fog.svg"
+
+
+            }
+            else if (data.weather[0].main === "Tornado") {
+                weather_img.src = "pic/tornado.svg"
+
+
+            }
+            else {
+                weather_img.src = "pic/clear-day.svg"
+            }
+
 
             console.log(data)
         })
@@ -32,5 +84,82 @@ navigator.geolocation.getCurrentPosition((location)=>{
             console.log(err)
         })
 
-    })
+})
+
+
+
+let getLocation = document.getElementById("input")
+
+let getWeather = () => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${getLocation.value}&appid=50bf2c0c611fd572a45bb2a668f207f7&units=metric`)
+    .then(data => data.json())
+    .then((data) => {
+            let weather_img = document.getElementById("weather_img")
+            country.innerHTML = data.sys.country;
+            city.innerHTML = `${data.name},${data.sys.country}`
+            date.innerHTML = moment().format("ll");
+            temp.innerHTML = Math.round(data.main.temp) + "°C";
+            weather.innerHTML = data.weather[0].main
+            feels_like.innerHTML = Math.round(data.main.feels_like) + "°C";
+            humitidy.innerHTML = data.main.humidity + "%"
+            wind_speed.innerHTML = data.wind.speed + "km/h"
+            clouds.innerHTML = data.clouds.all + "%"
+            if (data.weather[0].main == "Thunderstorm") {
+                weather_img.src = "pic/thunderstorms-rain.svg";
+            }
+            else if (data.weather[0].main == "Clouds") {
+                weather_img.src = 'pic/cloudy.svg';
+                
+                
+            }
+            else if (data.weather[0].main === "Rain") {
+                weather_img.src = "pic/extreme-night-rain.svg"
+
+            }
+            else if (data.weather[0].main === "Snow") {
+                weather_img.src = "pic/overcast-night-snow.svg"
+
+            }
+            else if (data.weather[0].main === "Mist") {
+                weather_img.src = "pic/mist.svg"
+
+
+            }
+            else if (data.weather[0].main === "Smoke") {
+                weather_img.src = "pic/smoke.svg"
+
+
+
+            }
+            else if (data.weather[0].main === "Haze") {
+                weather_img.src = "pic/haze.svg"
+
+
+            }
+            else if (data.weather[0].main === "Dust") {
+                weather_img.src = "pic/dust.svg"
+
+
+            }
+            else if (data.weather[0].main === "Fog") {
+                weather_img.src = "pic/fog.svg"
+
+
+            }
+            else if (data.weather[0].main === "Tornado") {
+                weather_img.src = "pic/tornado.svg"
+
+
+            }
+            else {
+                weather_img.src = "pic/clear-day.svg"
+            }
+            getLocation.value = ""
+
+            
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 
