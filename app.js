d0abd325a17b3a6953351cbc871dc4200ce35309
Ahmed Karim <121400813+Muhammadahmed2001@users.noreply.github.com>
2023-07-04 19:27:24 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let country = document.getElementById("country")
 let date = document.getElementById("date")
 let temp = document.getElementById("temp")
 let weather = document.getElementById("weather")
+let feels_like = document.getElementById("feels_like")
+let humitidy = document.getElementById("humitidy")
+let wind_speed = document.getElementById("wind_speed")
+let clouds = document.getElementById("clouds")
 
 navigator.geolocation.getCurrentPosition((location)=>{
     let lat = location.coords.latitude
@@ -17,9 +21,12 @@ navigator.geolocation.getCurrentPosition((location)=>{
             date.innerHTML = moment().format("MMM Do YY");
             temp.innerHTML = Math.round(data.main.temp)+"°C";
             weather.innerHTML = data.weather[0].main
+            feels_like.innerHTML = Math.round(data.main.feels_like)+"°C";
+            humitidy.innerHTML = data.main.humidity +"%"
+            wind_speed.innerHTML = data.wind.speed +"km/h"
+            clouds.innerHTML = data.clouds.all +"%"
 
-
-            console.log()
+            console.log(data)
         })
         .catch((err) => {
             console.log(err)
@@ -27,6 +34,3 @@ navigator.geolocation.getCurrentPosition((location)=>{
 
     })
 
-// navigator.geolocation.getCurrentPosition((location)=>{
-//     console.log(location.coords)
-// })
